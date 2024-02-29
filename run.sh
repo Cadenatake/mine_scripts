cec061aa6235a11ee724de621e12ed9f0b14d1cf
goodcode <f15K6023@kgu.jp>
2024-03-01 00:37:36 +0900

    push JRE path relatived

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd ${SCRIPT_DIR}
 
 echo ${LOGDATASTAMP} > ${LOGPATH}
-/usr/lib/jvm/java-17-openjdk-17.0.1.0.12-2.el8_5.aarch64/bin/java -Xmx1g -Xms1g -jar ${SERVERJAR} -nogui -eraseCache | tee -a ${LOGPATH}
+java -Xmx1g -Xms1g -jar ${SERVERJAR} -nogui -eraseCache | tee -a ${LOGPATH}
 
 exit 0
